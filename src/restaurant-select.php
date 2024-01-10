8be76376ae52d77689ff88eb66fe09e022f755fa
Kairi220 <oyakairi@outlook.jp>
2024-01-10 11:00:12 +0900

    Update restaurant-select.php

--- src/restaurant-select.php
+++ src/restaurant-select.php
@@ -8,9 +8,17 @@
 <body>
 <h2>飲食店情報管理メニュー</h2>
     <table>
-        <tr><th>飲食店ID</th><th>飲食店名</th><th>カテゴリ</th></tr>
-            <?php
-                
-            ?>
+        <?php
+            $pdo=new PDO($connect, USER, PASS);
+            $sql=$pdo->query('select * from restaurant');
+            foreach($sql as $row){              
+                echo '<tr>';
+                echo '<td>', $row['id'], '</td>';
+                echo '<td>', $row['name'], '</td>';
+                echo '<td>', $row['category_id'], '</td>';
+                echo '</tr>';
+                echo "\n";
+            }
+        ?>
     </table>
 </html>
