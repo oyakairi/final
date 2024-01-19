82120ce75ef235dde4937f8f840119caa4c720d9
Kairi220 <oyakairi@outlook.jp>
2024-01-20 00:19:14 +0900

    Update restaurant-update.php

--- src/restaurant-update.php
+++ src/restaurant-update.php
@@ -9,7 +9,6 @@
 <body>
     <h2>飲食店情報管理メニュー</h2>
     <a href="restaurant-menu.php">ホームへ戻る</a>
-    <form action="restaurant-update-result.php">
     <table>
     <tr><th></th><th>ID</th><th>飲食店名</th><th>カテゴリID</th></tr>
     <?php
@@ -17,17 +16,16 @@
         $sql1=$pdo->query('select * from restaurant');
         foreach($sql1 as $row){              
             echo '<tr>';
-            echo '<td>';
-            echo '<input type="radio" value="', $row['id'],'">';
-            echo '</td> ';
+            echo '<form action="restaurant-update-result.php">';
             echo '<td>', $row['id'], '</td>';
             echo '<td>';
 		    echo '<input type="text" name="name" value="', $row['name'], '">';
 		    echo '</td> ';
             echo '<td>';
-            echo '<input type="text" name="name" value="', $row['category_id'], '">';
+            echo '<input type="text" name="category" value="', $row['category_id'], '">';
             echo '</td> ';
             echo '<td>';
+            echo '<input type="hidden" name="id" value="', $row['id'], '">';
             echo '<input type="submit" value="更新">';
             echo '</td>';
             echo '</tr>';
