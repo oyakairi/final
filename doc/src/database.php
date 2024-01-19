02bf9e82ec269a49fe511b354e57785c867258dd
Kairi220 <oyakairi@outlook.jp>
2024-01-19 19:00:53 +0900

    delete

--- doc/src/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const SERVER = 'mysql220.phy.lolipop.lan';
-    const DBNAME = 'LAA1517799-final';
-    const USER = 'LAA1517799';
-    const PASS = 'Pass0724';
-
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
