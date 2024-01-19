1248c1380d5ac0bc982ec2d452eaf48b79ccd2b3
Kairi220 <oyakairi@outlook.jp>
2024-01-19 23:42:26 +0900

    Update restaurant-delete.php

--- src/restaurant-delete.php
+++ src/restaurant-delete.php
@@ -9,6 +9,23 @@
 <body>
     <h2>飲食店情報管理メニュー</h2>
     <a href="restaurant-menu.php">ホームへ戻る</a>
-    <?php require 'restaurant.php'; ?>
+    <form action="restaurant-delete-result.php">
+    <table>
+    <tr><th>ID</th><th>飲食店名</th><th>カテゴリID</th></tr>
+    <?php
+        $pdo=new PDO($connect, USER, PASS);
+        $sql=$pdo->query('select * from restaurant');
+        foreach($sql as $row){              
+            echo '<tr>';
+            echo '<td>', $row['id'], '</td>';
+            echo '<td>', $row['name'], '</td>';
+            echo '<td>', $row['category_id'], '</td>';
+            echo '</tr>';
+            echo "\n";
+        }
+    ?>
+</table>
+<input type="submit" value="削除">
+</form>
 </body>
 </html>
