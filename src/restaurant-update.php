e7387f1d3277a84748abefa1826a9bff6a907e38
Kairi220 <oyakairi@outlook.jp>
2024-01-20 01:07:48 +0900

    Update restaurant-update.php

--- src/restaurant-update.php
+++ src/restaurant-update.php
@@ -16,23 +16,21 @@
         $sql1=$pdo->query('select * from restaurant');
         foreach($sql1 as $row){              
             echo '<tr>';
-            echo '<form action="restaurant-update-result.php">';
             echo '<td>', $row['id'], '</td>';
+            echo '<td>', $row['name'], '</td>';
+            echo '<td>', $row['category_id'], '</td>';
             echo '<td>';
-		    echo '<input type="text" name="name" value="', $row['name'], '">';
-		    echo '</td> ';
-            echo '<td>';
-            echo '<input type="text" name="category" value="', $row['category_id'], '">';
-            echo '</td> ';
-            echo '<td>';
+            echo '<form action="restaurant-update-result.php">';
             echo '<input type="hidden" name="id" value="', $row['id'], '">';
+            echo '<input type="hidden" name="name" value="', $row['name'], '">';
+            echo '<input type="hidden" name="category" value="', $row['category_id'], '">';
             echo '<input type="submit" value="更新">';
+            echo '</form>';
             echo '</td>';
             echo '</tr>';
             echo "\n";
         }
     ?>
     </table>
-    </form>
 </body>
 </html>
