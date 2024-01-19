310690468514eda70d65404650cea6a63b38ce74
Kairi220 <oyakairi@outlook.jp>
2024-01-20 01:37:21 +0900

    Update restaurant-update.php

--- src/restaurant-update.php
+++ src/restaurant-update.php
@@ -10,27 +10,25 @@
     <h2>飲食店情報管理メニュー</h2>
     <a href="restaurant-menu.php">ホームへ戻る</a>
     <table>
-    <tr><th>ID</th><th>飲食店名</th><th>カテゴリID</th><th></th></tr>
+    <tr><th></th><th>ID</th><th>飲食店名</th><th>カテゴリID</th><th></th></tr>
     <?php
         $pdo=new PDO($connect, USER, PASS);
         $sql=$pdo->query('select * from restaurant');
         foreach($sql as $row){              
             echo '<tr>';
+            echo '<td><input type="radio" name="id" value="', $row['id'], '"></td>';
             echo '<td>', $row['id'], '</td>';
             echo '<td>', $row['name'], '</td>';
             echo '<td>', $row['category_id'], '</td>';
             echo '<td>';
             echo '<form action="restaurant-update-result.php">';
-            echo '<input type="hidden" name="id" value="', $row['id'], '">';
-            echo '<input type="hidden" name="name" value="', $row['name'], '">';
-            echo '<input type="hidden" name="category" value="', $row['category_id'], '">';
-            echo '<input type="submit" value="更新">';
-            echo '</form>';
             echo '</td>';
             echo '</tr>';
             echo "\n";
         }
     ?>
     </table>
+    <input type="submit" value="更新">
+    </form>
 </body>
 </html>
